a4c431b92b3f074a18d5976d11819f13ebe72141
steelemeg <71856754+steelemeg@users.noreply.github.com>
2021-11-07 22:33:49 -0600

    Heroku mods

--- test_requests.py
+++ test_requests.py
@@ -11,3 +11,9 @@ channel.queue_declare(queue='res_google_images')
 channel.basic_publish(exchange='', routing_key='req_google_images', \
                       body='{"image_parameters": ["mars rover", "xkcd"], "num_images": 2}')
                       #body='i am not json mwaha')
+
+def testRequest(channel, method, properties, req_body):
+    print(req_body)
+# Testing
+channel.basic_consume(queue='res_google_images', on_message_callback=testRequest, auto_ack=True)
+channel.start_consuming()
