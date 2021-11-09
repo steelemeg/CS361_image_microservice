3b13e5e3f1aeb401ee1354ed33b3cc77bcd8f0e2
steelemeg <71856754+steelemeg@users.noreply.github.com>
2021-11-08 18:31:27 -0600

    Refactoring

---
+++ client_requests.py
@@ -0,0 +1,54 @@
+# Using template code from https://www.cloudamqp.com/docs/python.html
+# Sample client interaction code
+
+import pika, os, json, uuid
+
+class ImageRequests(object):
+
+    def __init__(self):
+
+        # Configure connection
+        self.url = os.environ.get('CLOUDAMQP_URL')
+        self.parameters = pika.URLParameters(self.url)
+        self.connection = pika.BlockingConnection(self.parameters)
+
+        self.channel = self.connection.channel()
+
+        # Results queue can be any value at the user's discretion. This is where the client should
+        # listen for image responses, and where the image server will publish responses.
+        result = self.channel.queue_declare(queue='google_images_Megan', exclusive=False)
+        self.callback_queue = result.method.queue
+
+        # Start listening to the callback queue
+        self.channel.basic_consume(
+            queue=self.callback_queue,
+            on_message_callback=self.on_response,
+            auto_ack=True)
+
+    def on_response(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            self.response = body
+
+    def call(self, n):
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+        self.channel.basic_publish(
+            exchange='',
+            routing_key='google_images',
+            properties=pika.BasicProperties(
+                reply_to=self.callback_queue,
+                correlation_id=self.corr_id,
+            ),
+            body=n)
+        while self.response is None:
+            self.connection.process_data_events()
+        return self.response
+
+
+google_image_client = ImageRequests()
+
+# Send a test message
+message = {'image_parameters': ["mars rover", "xkcd"], "num_images": '3'}
+response = google_image_client.call(json.dumps(message))
+print("Printing response sent to client from server:")
+print(json.loads(response))
---
+++ server_responses.py
@@ -0,0 +1,52 @@
+# Based on code from https://www.cloudamqp.com/docs/python.html
+
+import pika, os, json
+# Import the parser code and instantiate.
+from getGoogleImages import GoogleImages
+image_fetcher = GoogleImages()
+
+
+# Access the CLOUDAMQP_URL environment variable
+# and parse it (fallback to localhost)
+url = os.environ.get('CLOUDAMQP_URL')
+params = pika.URLParameters(url)
+
+# Configure the connection.
+connection = pika.BlockingConnection(params)
+channel = connection.channel()
+channel.queue_declare(queue='google_images')
+
+def on_request(ch, method, properties, req_body):
+    json_response = {'success': True}
+    num_images = 10
+
+    # Check if the requests body parses as valid JSON.
+    try:
+        json_request = json.loads(req_body)
+    except ValueError as e:
+        json_response = {'success': False, 'error_message': 'Request body did not contain not valid JSON'}
+    else:
+        # Parse the request and prepare the search terms. image_parameters is required, num_images is optional.
+        if 'image_parameters' not in json_request:
+            json_response = {'success': False, 'error_message': 'Missing image_parameters. This is a required field'}
+
+        else:
+            if 'num_images' in json_request:
+                num_images = json_request['num_images']
+            image_parameters = json_request['image_parameters']
+            results = image_fetcher.image_query(image_parameters, num_images)
+            json_response['images'] = results
+            ch.basic_publish(exchange='',
+                             routing_key=properties.reply_to,
+                             properties=pika.BasicProperties(correlation_id= \
+                                                                 properties.correlation_id),
+                             body=json.dumps(json_response))
+            ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume('google_images', on_message_callback=on_request)
+
+print(' [*] Waiting for messages:')
+channel.start_consuming()
+connection.close()
